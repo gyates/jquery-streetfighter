624e31da8a04c25376d929970ff6d3ce870fe089
gyates <gavinyat@gmail.com>
2014-05-11 10:49:44 -0700

    final

---
+++ js/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').finish().show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().
+    	animate({'left': '300px'}, 500,
+    		function() {
+    			$(this).hide();
+    			$(this).css('left', '-212px');
+        });
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+
+  $(document).keydown(function(event) {
+    var key = String.fromCharCode(event.which);
+    if (key == 'X') {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }
+  })
+    .keyup(function(event) {
+      var key = String.fromCharCode(event.which);
+      if (key =='X') {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+      }
+  });
+
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
